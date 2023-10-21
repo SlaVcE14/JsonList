71e24c39492c338d5e75599fe36754f7dfc72e5d
SlaVcE <102253539+SlaVcE14@users.noreply.github.com>
2023-10-22 00:36:48 +0200

    update Gson

--- app/src/main/java/com/sjapps/about/LibraryList.java
+++ app/src/main/java/com/sjapps/about/LibraryList.java
@@ -1,12 +1,13 @@
 package com.sjapps.about;
 
+import com.google.gson.internal.GsonBuildConfig;
 import com.sjapps.library.BuildConfig;
 
 public class LibraryList extends ListGenerator{
     @Override
     public void init() {
-        addItem("gson","2.8.9", "https://github.com/google/gson");
         addItem("SJ Dialog", BuildConfig.VERSION_NAME, "https://github.com/SlaVcE14/SJ-Dialog");
+        addItem("gson", GsonBuildConfig.VERSION, "https://github.com/google/gson");
     }
 }
 
