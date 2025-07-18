a9d0abed5a82ca0b4934c6b88a45b5aeadac92c5
SlaVcE <slavce14.apps@gmail.com>
2025-07-19 00:36:22 +0200

    update WebManager

--- core/src/main/java/com/sj14apps/jsonlist/core/controllers/WebManager.java
+++ core/src/main/java/com/sj14apps/jsonlist/core/controllers/WebManager.java
@@ -1,10 +1,59 @@
 package com.sj14apps.jsonlist.core.controllers;
 
-public interface WebManager {
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.jetbrains.annotations.NotNull;
 
-    void getFromUrl(String url, WebCallback webCallback);
+import java.io.IOException;
 
-    interface WebCallback {
+
+public class WebManager {
+
+    public void getFromUrl(String url, WebCallback webCallback){
+        if (url.trim().isEmpty())
+            return;
+
+        if (!url.startsWith("http"))
+            url = "https://" + url;
+
+        OkHttpClient client = new OkHttpClient();
+        Request request;
+        try {
+            request = new Request.Builder()
+                    .url(url)
+                    .build();
+
+        }catch (IllegalArgumentException e){
+            webCallback.onInvalidURL();
+            return;
+        }
+
+        client.newCall(request).enqueue(new Callback() {
+
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                webCallback.onFailure();
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                if (response.body() == null) {
+                    webCallback.onFailure(response.code());
+                    return;
+                }
+                webCallback.onResponse(response.body().string());
+
+            }
+        });
+        webCallback.onStarted();
+    }
+
+    public interface WebCallback {
+        void onStarted();
+        void onInvalidURL();
         void onResponse(String data);
         void onFailure();
         void onFailure(int code);
